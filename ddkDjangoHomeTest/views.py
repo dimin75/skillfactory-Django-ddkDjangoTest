f05aec963aed7b1ff8b20b635e83d0810546da40
dimin75 <dmitry.kaplunenko@gmail.com>
2023-08-11 20:43:09 +1000

    Django test task D1.6

---
+++ ddkDjangoHomeTest/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+
+def page1(request):
+    return render(request, 'page1.html')
+
+def page2(request):
+    return render(request, 'page2.html', {'flatpage': {'content': 'Sample content'}})
+
+def page3(request):
+    return render(request, 'page3.html')
+
+@staff_member_required
+def admin_page(request):
+    return render(request, 'admin_page.html')
+
+# Create your views here.
